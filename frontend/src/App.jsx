635d0dcc41374b3f35d5b4053d058e4bc0d2d25b
hpatni29 <himanshu.patni16@iimranchi.ac.in>
2025-08-28 17:53:46 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,19 @@ function App() {
   return (
     <div className="max-w-5xl mx-auto p-6 font-sans">
       <h1 className="text-3xl font-bold mb-6 text-center">Digamber Jain Community</h1>
+      
       <AIQuestion />
+      
       <div className="my-6 border-b"></div>
+      
       <Gurus />
+      
       <div className="my-6 border-b"></div>
+      
       <Temples />
+      
       <div className="my-6 border-b"></div>
+      
       <Users />
     </div>
   );
